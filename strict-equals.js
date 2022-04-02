5f8ba14eaf472b34734e9093ba3a06daf29bb441
Ilham Gunawan <50332051+ilhammrg@users.noreply.github.com>
2022-04-03 01:20:23 +0700

    Update strict-equals.js

--- strict-equals.js
+++ strict-equals.js
@@ -1,5 +1,5 @@
+// Disclaimer: This function is useless, just another implementation of strict equals (===)
 // This strictEquals function should return the same output as a === b
-// TODO: add test cases for all possible inputs
 
 function strictEquals(a, b) {
     if (Object.is(a, b)) {
